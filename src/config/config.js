81992585ed98afd04bcff226386cb5d38e22f49b
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-10-25 11:08:38 -0300

    advances

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
+require('pg')
+require('pg-hstore')
 require('dotenv').config();
 
 module.exports = {
@@ -8,6 +10,12 @@ module.exports = {
     use_env_variable: "DATABASE_URL",
   },
   "production": {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: "DATABASE_URL_PRODUCTION",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,8 @@
+require('pg')
+require('pg-hstore')
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL || process.env.DATABASE_URL_PRODUCTION)
 
 module.exports = sequelize;
